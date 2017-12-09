7ee5c10787eac0884802387eee294c31146838b3
Jaylyn Stoesz <jaylynstoesz@Jaylyns-MacBook-Pro-2.local>
2017-12-08 22:40:32 -0700

    balance function

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -11,27 +11,49 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-      if (c > (r + 1)) {
-        throw new NoSuchElementException
-      }
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c > (r + 1)) {
+      throw new NoSuchElementException
+    }
 
-      if (c == 0 || c == r) {
-        1
+    // if col is the beginning or end of the row, its value is 1
+    if (c == 0 || c == r) {
+      1
+    // add values of index and one index prior fron row above
+    } else {
+      pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
+  }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    // filter out all the extra characters
+    val p = chars.filter(c => c.toString == ")" || c.toString == "(")
+
+    if (p.length <= 1) {
+      p.isEmpty
+    } else {
+      if (p.startsWith("(")) {
+        // drop open-close pairs from the beginning of the string
+        if (p.tail.startsWith(")")) {
+          balance(p.tail.tail)
+          // otherwise rotate the characters
+        } else {
+          balance(List(p.head) ::: p.tail.tail ::: List(p.tail.head))
+        }
+        // rotate characters
       } else {
-        pascal(c, r - 1)  + pascal(c - 1, r - 1)
+        balance(p.tail ::: List(p.head))
       }
     }
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
+  }
 
   /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = 0
-  }
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = 0
+}
